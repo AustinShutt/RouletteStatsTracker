eec0c6e1c28a16a4efccd9a4f5eed3b2ef3fb721
AustinShutt <69128774+AustinShutt@users.noreply.github.com>
2022-05-19 21:16:58 -0400

    Added each class for shell constructor dependency injection

--- RouletteStatsTracker/MauiProgram.cs
+++ RouletteStatsTracker/MauiProgram.cs
@@ -3,6 +3,7 @@
 using RouletteStatsTracker.Models;
 using RouletteStatsTracker.Views;
 using RouletteStatsTracker.Pages;
+using RouletteStatsTracker.ViewModels;
 public static class MauiProgram
 {
 	public static MauiApp CreateMauiApp()
@@ -16,13 +17,36 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 
+		/**
+		 * Summary:
+		 * Adding these as singletons with the builder allows constructor dependency injection through the
+		 * shell. If we require access to a class of a particular type, try to request it through the 
+		 * constructor. If the class is added here, it will be passed to the constructor automatically.
+		 */
+
 		builder.Services.AddSingleton<App>();
+		//Add Model Singletons
 		builder.Services.AddSingleton<DataStore>();
 		builder.Services.AddSingleton<Settings>();
-		builder.Services.AddSingleton<AmericanInputView>();
-		builder.Services.AddSingleton<EuropeanInputView>();
-		builder.Services.AddSingleton<ValueInputPage>();
 
-		return builder.Build();
+		//Add ViewModel Singletons
+		builder.Services.AddSingleton<AmericanInputViewModel>();
+        builder.Services.AddSingleton<EuropeanInputViewModel>();
+        builder.Services.AddSingleton<AmericanStatsViewModel>();
+        builder.Services.AddSingleton<EuropeanInputViewModel>();
+		builder.Services.AddSingleton<FlowViewModel>();
+
+        //Add View Singletons
+        builder.Services.AddSingleton<AmericanInputView>();
+		builder.Services.AddSingleton<EuropeanInputView>();
+		builder.Services.AddSingleton<AmericanStatsView>();
+        builder.Services.AddSingleton<EuropeanStatsView>();
+		//Add Page Singletons
+        builder.Services.AddSingleton<ValueInputPage>();
+        builder.Services.AddSingleton<FlowPage>();
+        builder.Services.AddSingleton<StatsPage>();
+        builder.Services.AddSingleton<SettingsPage>();
+
+        return builder.Build();
 	}
 }
