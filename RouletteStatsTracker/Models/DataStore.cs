7292af967715dc91634a7ce0d91771eb9c1475f5
AustinShutt <69128774+AustinShutt@users.noreply.github.com>
2022-05-19 22:25:29 -0400

    Added DataStore Properties

--- RouletteStatsTracker/Models/DataStore.cs
+++ RouletteStatsTracker/Models/DataStore.cs
@@ -8,12 +8,25 @@ namespace RouletteStatsTracker.Models
 {
     public class DataStore
     {
-        public bool American { get; set; }
-        public bool European { get; set; }
+        public int Red { get;  set; }
+        public int Black { get;  set; }
+        public int Even { get;  set; }
+        public int Odd { get;  set; }
+        public int Low { get;  set; }
+        public int High { get;  set; }
+        public int Column1 { get; set; }
+        public int Column2 { get; set; }
+        public int Column3 { get; set; }
+        public int Third1 { get; set; }
+        public int Third2 { get; set; }
+        public int Third3 { get; set; }
 
-        DataStore()
+        public int[] numberArray;
+
+        public DataStore()
         {
-            American = true; ;
+            //0 , 1-36, 00
+            numberArray = new int[38]; 
         }
     }
 }
